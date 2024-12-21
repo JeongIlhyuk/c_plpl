9355444497975856918b0d6b8801264b3a45d11a
explodeTheComputer <dnfjddk2@gmail.com>
2024-12-21 14:45:38 +0900

    init

---
+++ helloworld.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void swap1(int &a,int &b){
+    int temp=a;
+    a=b;
+    b=temp;
+}
+
+int main(){
+    int v1=10,v2=20;
+
+    swap1(v1,v2);
+    cout<<"v1:"<<v1<<' '<<"v2:"<<v2<<endl;
+    return 0;
+}
